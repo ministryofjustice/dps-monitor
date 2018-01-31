f6433091b98b7acfc4edf655f37cabf63a90c6e4
Michael Willis <michael.willis@syscon.net>
2018-01-31 12:47:39 +0000

    Add Keyworker UI

--- jobs/circle_ci.rb
+++ jobs/circle_ci.rb
@@ -4,7 +4,8 @@ require 'httparty'
 projects = [
   { vcs: 'bitbucket', user: 'cool_syscon_team', repo: 'mobile', branch: 'nomis-api'},
   { vcs: 'bitbucket', user: 'cool_syscon_team', repo: 'elite2-web', branch: 'master'},
-  { vcs: 'github', user: 'ministryofjustice', repo: 'nomis-api', branch: 'dev'}
+  { vcs: 'github', user: 'ministryofjustice', repo: 'nomis-api', branch: 'dev'},
+  { vcs: 'github', user: 'ministryofjustice', repo: 'keyworker-ui', branch: 'master'}
 ]
 
 def duration(time)
