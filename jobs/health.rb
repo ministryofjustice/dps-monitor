bf98d91a631852821652d8d81b60a065154d8f13
Michael Willis <michael.willis@digital.justice.gov.uk>
2019-01-11 15:14:28 +0000

    update health check

--- jobs/health.rb
+++ jobs/health.rb
@@ -50,7 +50,7 @@ servers = [
     {name: 'oauth2-preprod',            versionUrl: 'https://gateway.preprod.nomis-api.service.hmpps.dsd.io/auth/info', url: 'https://gateway.preprod.nomis-api.service.hmpps.dsd.io/auth/health', method: 'http'},
 
     {name: 'elite2-stage',              versionUrl: 'https://gateway.t2.nomis-api.hmpps.dsd.io/elite2api/info', url: 'https://gateway.t2.nomis-api.hmpps.dsd.io/elite2api/health', method: 'http'},
-    {name: 'keyworker-api-stage',       url: 'https://keyworker-api-stage.hmpps.dsd.io/health', method: 'http'},
+    {name: 'keyworker-api-stage',       versionUrl: 'https://keyworker-api-stage.hmpps.dsd.io/info', url: 'https://keyworker-api-stage.hmpps.dsd.io/health', method: 'http'},
     {name: 'notm-stage',                url: 'https://notm-stage.hmpps.dsd.io/health', method: 'http'},
     {name: 'omic-ui-stage',             url: 'https://omic-stage.hmpps.dsd.io/health', method: 'http'},
     {name: 'psh-stage',                 url: 'https://prisonstaffhub-stage.hmpps.dsd.io/health', method: 'http'},
